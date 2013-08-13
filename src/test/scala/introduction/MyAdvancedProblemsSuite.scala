307aeb80fb62f8c785110173f75895d90561605f
mariatsji <sjur.millidahl@gmail.com>
2013-08-13 16:44:20 +0200

    Lagde supress av test-stacktrace (cherry picked from commit 05dd39c)

--- src/test/scala/introduction/MyAdvancedProblemsSuite.scala
+++ src/test/scala/introduction/MyAdvancedProblemsSuite.scala
@@ -1,25 +1,25 @@
 package introduction
 
-import org.scalatest.FunSuite
+import funsuitehelper.FunSuiteHelper
 
-class MyAdvancedProblemsSuite extends FunSuite{
+class MyAdvancedProblemsSuite extends FunSuiteHelper{
 
   test("should give empty list when duplicating empty list") {
-    assert(MyAdvancedProblems.duplicate(List()) == List())
+    checkImplemented{MyAdvancedProblems.duplicate(List()) == List()}
   }
 
   test("should duplicate list of elements containing one element") {
-    assert(MyAdvancedProblems.duplicate(List('a)) == List('a,'a))
+    checkImplemented{MyAdvancedProblems.duplicate(List('a)) == List('a,'a)}
   }
 
   test("should duplicate list of elements containing four unique elements") {
-    assert(MyAdvancedProblems.duplicate(List('a,'b,'c,'d)) == List('a,'a,'b,'b,'c,'c,'d,'d))
+    checkImplemented{MyAdvancedProblems.duplicate(List('a,'b,'c,'d)) == List('a,'a,'b,'b,'c,'c,'d,'d)}
   }
 
   test("pack consecutive duplicates of list elements into sublists") {
-    assert(MyAdvancedProblems.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ==
+    checkImplemented{MyAdvancedProblems.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ==
       List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)
-    ))
+    )}
   }
 
 
