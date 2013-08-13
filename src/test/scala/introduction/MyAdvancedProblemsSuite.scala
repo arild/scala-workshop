38e8812cde31f32cb93ed27d3f77f66833c63c4f
Sjur Millidahl <sjur.millidahl@bekk.no>
2013-08-13 17:30:56 +0200

    Gikk bort fra boolean asserts (cherry picked from commit 2f83237)

--- src/test/scala/introduction/MyAdvancedProblemsSuite.scala
+++ src/test/scala/introduction/MyAdvancedProblemsSuite.scala
@@ -5,21 +5,21 @@ import org.scalatest.matchers.ShouldMatchers
 
 class MyAdvancedProblemsSuite  extends FlatSpecHelper with ShouldMatchers {
 
-  it should("should give empty list when duplicating empty list") in {
-    checkImplemented{MyAdvancedProblems.duplicate(List()) == List()}
+  it should "should give empty list when duplicating empty list" in {
+    checkImplemented{MyAdvancedProblems.duplicate(List()) should equal (List())}
   }
 
-  it should("should duplicate list of elements containing one element") in {
-    checkImplemented{MyAdvancedProblems.duplicate(List('a)) == List('a,'a)}
+  it should "should duplicate list of elements containing one element"  in {
+    checkImplemented{MyAdvancedProblems.duplicate(List('a)) should equal (List('a,'a))}
   }
 
-  it should("should duplicate list of elements containing four unique elements") in {
-    checkImplemented{MyAdvancedProblems.duplicate(List('a,'b,'c,'d)) == List('a,'a,'b,'b,'c,'c,'d,'d)}
+  it should "should duplicate list of elements containing four unique elements" in {
+    checkImplemented{MyAdvancedProblems.duplicate(List('a,'b,'c,'d)) should equal (List('a,'a,'b,'b,'c,'c,'d,'d))}
   }
 
-  it should("pack consecutive duplicates of list elements into sublists") in {
-    checkImplemented{MyAdvancedProblems.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ==
-      List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)
+  it should "pack consecutive duplicates of list elements into sublists" in {
+    checkImplemented{MyAdvancedProblems.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal (
+      List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
     )}
   }
 }
--- src/test/scala/introduction/MyIntroProblemsTest.scala
+++ src/test/scala/introduction/MyIntroProblemsTest.scala
@@ -8,34 +8,38 @@ import org.scalatest.matchers.ShouldMatchers
 
 class MyIntroProblemsTest extends FlatSpecHelper with ShouldMatchers {
 
-  it should("should compute square") in {
-    checkImplemented{MyIntroProblems.computeSquare(4) == 16}
+  it should "should compute square" in {
+    checkImplemented{MyIntroProblems.computeSquare(4) should equal (16)}
   }
 
-  it should("should find last element") in {
-    checkImplemented{MyIntroProblems.findLastElement(List(1, 2, 3)) == 3}
+  it should "should find last element" in {
+    checkImplemented{MyIntroProblems.findLastElement(List(1, 2, 3)) should equal (3)}
   }
 
-  it should("should find two last elements") in {
-    checkImplemented{MyIntroProblems.findTwoLastElements(List(1, 2, 3)) == List(2, 3)}
+  it should "should find two last elements" in {
+    checkImplemented{MyIntroProblems.findTwoLastElements(List(1, 2, 3)) should equal (List(2, 3))}
   }
 
-  it should("should find even numbers") in {
-    checkImplemented{MyIntroProblems.findEvenNumbers(List(1, 2, 3, 4, 6, 7)) == List(2, 4, 6)}
+  it should "should find even numbers" in {
+    checkImplemented{MyIntroProblems.findEvenNumbers(List(1, 2, 3, 4, 6, 7)) should equal (List(2, 4, 6))}
   }
 
-  it should("should find sublist from predicate") in {
-    checkImplemented{MyIntroProblems.findSublistFromPredicate(List(1, 2, 3, 4, 5), x => x > 3) == List(4, 5)}
-    checkImplemented{MyIntroProblems.findSublistFromPredicate(List(1, 2, 3, 4, 5), x => x % 2 == 1) == List(1, 3, 5)}
+  it should "should find sublist from predicate" in {
+    checkImplemented{
+      MyIntroProblems.findSublistFromPredicate(List(1, 2, 3, 4, 5), x => x > 3) should equal (List(4, 5))
+      MyIntroProblems.findSublistFromPredicate(List(1, 2, 3, 4, 5), x => x % 2 == 1) should equal (List(1, 3, 5))
+    }
   }
 
-  it should("should sum of all max factors") in {
+  it should "should sum of all max factors" in {
     val work = List(new FactorNumber(21L), new FactorNumber(49L), new FactorNumber(12L))
-    checkImplemented{MyIntroProblems.findSumOfAllMaxFactors(work) == 20L}
+    checkImplemented{MyIntroProblems.findSumOfAllMaxFactors(work) should equal (20L)}
   }
 
-  it should("should compute square of int or string") in {
-    checkImplemented{MyIntroProblems.computeSquareOfIntOrString(4) == 16}
-    checkImplemented{MyIntroProblems.computeSquareOfIntOrString("4") == 16}
+  it should "should compute square of int or string" in {
+    checkImplemented{
+      MyIntroProblems.computeSquareOfIntOrString(4) should equal(16)
+      MyIntroProblems.computeSquareOfIntOrString("4") should equal(16)
+    }
   }
 }
