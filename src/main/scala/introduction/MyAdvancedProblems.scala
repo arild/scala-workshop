451ad8918791dd237e144227420d81edccf7bd6e
Arild Nilsen <arild.nilsen2@gmail.com>
2013-09-01 20:50:15 +0200

    renamed variable

--- src/main/scala/introduction/MyAdvancedProblems.scala
+++ src/main/scala/introduction/MyAdvancedProblems.scala
@@ -20,12 +20,12 @@ object MyAdvancedProblems {
   // Example:
   // scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
   // res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
-  def pack[A](ls: List[A]): List[List[A]] = {
-    val packed = ls takeWhile {
-      _ == ls.head
+  def pack[A](elements: List[A]): List[List[A]] = {
+    val packed = elements takeWhile {
+      _ == elements.head
     }
-    val next = ls dropWhile {
-      _ == ls.head
+    val next = elements dropWhile {
+      _ == elements.head
     }
     if (next == Nil)
       List(packed)
