f6e5eaeabe03905a14c51e2a3000589a0844b4cf
Arild Nilsen <arild.nilsen2@gmail.com>
2013-09-01 20:48:28 +0200

    renamed variable

--- src/main/scala/introduction/MyAdvancedProblems.scala
+++ src/main/scala/introduction/MyAdvancedProblems.scala
@@ -15,7 +15,7 @@ object MyAdvancedProblems {
   // Example:
   // scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
   // res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
-  def pack[A](ls: List[A]): List[List[A]] = {
+  def pack[A](elements: List[A]): List[List[A]] = {
     ???
   }
 
