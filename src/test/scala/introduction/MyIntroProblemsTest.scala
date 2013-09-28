71e97cfc2b154fed1c005b1b76d417e4987f55e7
Arild Nilsen <arild.nilsen2@gmail.com>
2013-09-28 17:37:32 +0200

    remove duplicate 'should' in test description. Add consintent formatting of tests (cherry picked from commit e9773ad)

--- src/test/scala/introduction/MyIntroProblemsTest.scala
+++ src/test/scala/introduction/MyIntroProblemsTest.scala
@@ -8,42 +8,44 @@ import org.scalatest.matchers.ShouldMatchers
 
 class MyIntroProblemsTest extends FlatSpecHelper with ShouldMatchers {
 
-  it should "should compute square" in {
-    checkImplemented { MyIntroProblems.computeSquare(4) should equal (16)}
+  it should "compute square" in {
+    checkImplemented {
+      MyIntroProblems.computeSquare(4) should equal (16)
+    }
   }
 
-  it should "should find last element" in {
+  it should "find last element" in {
     checkImplemented {
       MyIntroProblems.findLastElement(List(1, 2, 3)) should equal (3)
     }
   }
 
-  it should "should find two last elements" in {
+  it should "find two last elements" in {
     checkImplemented {
       MyIntroProblems.findTwoLastElements(List(1, 2, 3)) should equal (List(2, 3))
     }
   }
 
-  it should "should find even numbers" in {
+  it should "find even numbers" in {
     checkImplemented {
       MyIntroProblems.findEvenNumbers(List(1, 2, 3, 4, 6, 7)) should equal (List(2, 4, 6))
     }
   }
 
-  it should "should compute square on all numbers" in {
+  it should "compute square on all numbers" in {
     checkImplemented {
       MyIntroProblems.computeSquareOnAllNumbers(List(1, 2, 3, 4, 6, 7)) should equal (List(1, 4, 9, 16, 36, 49))
     }
   }
 
-  it should "should find sublist from predicate" in {
+  it should "find sublist from predicate" in {
     checkImplemented {
       MyIntroProblems.findSublistFromPredicate(List(1, 2, 3, 4, 5), x => x > 3) should equal (List(4, 5))
       MyIntroProblems.findSublistFromPredicate(List(1, 2, 3, 4, 5), x => x % 2 == 1) should equal (List(1, 3, 5))
     }
   }
 
-  it should "should compute sum of all max factors" in {
+  it should "compute sum of all max factors" in {
     val work1 = List(new FactorNumber(21L), new FactorNumber(49L), new FactorNumber(12L))
     val work2 = List(new FactorNumber(51L), new FactorNumber(81L))
     checkImplemented {
@@ -52,7 +54,7 @@ class MyIntroProblemsTest extends FlatSpecHelper with ShouldMatchers {
     }
   }
 
-  it should "should compute square of int or string" in {
+  it should "compute square of int or string" in {
     checkImplemented {
       MyIntroProblems.computeSquareOfIntOrString(4) should equal(16)
       MyIntroProblems.computeSquareOfIntOrString("4") should equal(16)
