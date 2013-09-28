164bc8e6350d4475675dff493b0c7da872d0faea
Arild Nilsen <arild.nilsen2@gmail.com>
2013-09-28 17:32:52 +0200

    add flatten problem and test

--- src/main/scala/introduction/MyAdvancedProblems.scala
+++ src/main/scala/introduction/MyAdvancedProblems.scala
@@ -15,6 +15,15 @@ object MyAdvancedProblems {
     //}
   }
 
+  // Flatten a nested list structure.
+  // Example:
+  // scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+  // res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+  def flatten(elements: List[Any]): List[Any] = elements flatMap {
+    case list: List[_] => flatten(list)
+    case element => List(element)
+  }
+
   // Pack consecutive duplicates of list elements into sublists.
   // If a list contains repeated elements they should be placed in separate sublists.
   // Example:
